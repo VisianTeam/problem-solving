6fb89cf276e80243026d67b925e93548eaced00a
BB6397 <BB6397@engie.com>
2022-09-05 16:46:32 +0200

    Add docstrings to tree sort

--- algorithms/tree_sort.py
+++ algorithms/tree_sort.py
@@ -1,9 +1,15 @@
+"""Implementation of tree sort in pure python
+
+reference: https://en.wikipedia.org/wiki/Tree_sort
+"""
 import numpy as np
 
 from utils.timeit import timeit
 
 
 class Tree:
+    """Representation of a binary search tree"""
+
     __slots__ = ["value", "node_inf", "node_sup"]
 
     def __init__(self, value: float, node_inf: "Tree" = None, node_sup: "Tree" = None):
@@ -12,6 +18,7 @@ class Tree:
         self.node_sup = node_sup
 
     def insert(self, value):
+        """Create new leaf for value"""
         if value < self.value:
             if self.node_inf is None:
                 self.node_inf = Tree(value)
@@ -24,6 +31,7 @@ class Tree:
             self.node_sup.insert(value)
 
     def to_list(self):
+        """Convert tree to sorted list"""
         return (
             (self.node_inf.to_list() if self.node_inf is not None else [])
             + [self.value]
@@ -32,12 +40,14 @@ class Tree:
 
 
 @timeit
-def basic_sort(l):
-    return sorted(l)
+def basic_sort(_list):
+    """Reference tree"""
+    return sorted(_list)
 
 
 @timeit
 def tree_sort(_list):
+    """Pure python implementation of tree sort"""
     tree = Tree(_list[0])
     for value in _list[1:]:
         tree.insert(value)
